93da412a45c17c6747515eaa21d2118a34ea4579
Nikolay Denisevich <n_denisevich@inbox.ru>
2020-01-18 21:44:21 +0300

    Update Population.cs

--- Task1/Population.cs
+++ Task1/Population.cs
@@ -4,6 +4,9 @@ namespace Task1
 {
     public static class Population
     {
+        /// <summary>
+        /// The main class that is being tested.
+        /// </summary>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
             // put your code here
